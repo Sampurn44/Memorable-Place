654896fc04b1e8ef794b8e5405a781c8c2a6c2f9
Sampurn Chouksey <sampurn10chouksey@gmail.com>
2023-07-22 03:10:27 +0530

    Add user_places provider and database to store user places.

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -16,8 +16,12 @@ class PlaceLocation {
 }
 
 class Place {
-  Place({required this.title, required this.image, required this.location})
-      : id = uuid.v4();
+  Place(
+      {required this.title,
+      required this.image,
+      required this.location,
+      String? id})
+      : id = id ?? uuid.v4();
   final String id;
   final String title;
   final File image;
--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -4,33 +4,54 @@ import 'package:favourite_place/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
+  @override
+  ConsumerState<PlacesScreen> createState() {
+    return _PlacesScreen();
+  }
+}
+
+class _PlacesScreen extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userplaces = ref.watch(userPlacesProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Places'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: ((context) => const AddPlacesScreen()),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.add)),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: PlacesList(
-          places: userplaces,
+        appBar: AppBar(
+          title: const Text('Your Places'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: ((context) => const AddPlacesScreen()),
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.add)),
+          ],
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : PlacesList(
+                        places: userplaces,
+                      ),
+          ),
+        ));
   }
 }
